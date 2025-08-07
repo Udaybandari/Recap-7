36c2665aefde1a93e1ef08055276bd7868cae942
Uday bandari <udaybandari593@gmail.com>
2025-08-07 18:07:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import UseReducer from "./components/UseReducer";
+import UseReducer2 from "./components/UseReducer2";
 
 const App = () => {
   return (
     <div>
       <UseReducer/>
+      <UseReducer2/>
     </div>
   )
 };
--- src/components/UseReducer2.jsx
+++ src/components/UseReducer2.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-
+import React, { useReducer } from "react";
+const initialstate={count:0}
+function reducer(state,action)
+{
+    switch(action.type)
+    {
+        case 'increment':
+            return {count:state.count+1};
+             case 'decrement':
+            return {count:state.count-1};
+             case 'reset':
+            return {count:0};
+            default:
+                return state;
+    }
+}
 const UseReducer2 = () => {
+     const [state,dispatch]=useReducer(reducer,initialstate);
   return (
-    <div>
-        
+    <div className="flex gap-5 m-5"> 
+        <p>{state.count}</p>
+        <button onClick={()=>dispatch({type:'increment'})}>INCREMENT</button>
+        <button onClick={()=>dispatch({type:'decrement'})}>DECREMENT</button>
+        <button onClick={()=>dispatch({type:'reset'})}>RESET</button>
     </div>
   )
 };
