b61e06ec7f690105e9bfb2a8805e8becd439103a
Uday bandari <udaybandari593@gmail.com>
2025-08-12 20:39:14 +0530

    updated features

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 const ADD_ITEM="ADD_ITEM";
 const REMOVE_ITEM="REMOVE_ITEM";
 const CHANGE_QUANTITY="CHANGE_QUANTITY";
@@ -18,12 +18,25 @@ function reducer(state,action)
       
      }
      ]
+     case REMOVE_ITEM:
+      return state.filter((t)=>t.id!=action.payload)
+       case CHANGE_QUANTITY:
+        return state.map(i=>
+          i.id===action.payload.id
+          ?{...i,quantity:action.payload.quantity}
+          :i
+        )
+        case CLEAR_CART:
+          return []
      default:
       return state;
     }
 }
 const ShoppingCart = () => {
-    const[state,dispatch]=useReducer(reducer,[]);
+    const[state,dispatch]=useReducer(reducer,[],()=>{const stored=localStorage.getItem("state");return stored?JSON.parse(stored):[]});
+    useEffect(()=>{
+      localStorage.setItem("state",JSON.stringify(state));
+    },[state])
     const handleAdd=()=>{
           const object={
   id: Date.now(),       
@@ -36,6 +49,12 @@ const ShoppingCart = () => {
 setQuantity(0);
 setPrice(0);
 
+    }
+    const handleDelete =(id)=>{
+      dispatch({type:REMOVE_ITEM,payload:id})
+    }
+    const handleChange =(id,e)=>{
+      dispatch({type:CHANGE_QUANTITY,payload:{id:id,quantity:e}})
     }
     const [name,setName]=useState("");
     const[quantity,setQuantity]=useState(0);
@@ -53,7 +72,7 @@ setPrice(0);
              <input className="w-33 focus:outline-none text-[15px]" value={quantity}  onChange={(e)=>setQuantity(e.target.value)}placeholder="Enter quantity.." type="number" min='1' step='1'  />
                <label >ADD PRICE:</label>
              <input className="w-33 focus:outline-none text-[15px]"  value={price}  onChange={(e)=>setPrice(e.target.value)} placeholder="Enter price.." type="number" min='1' step='1'  />
-             
+             <button onClick={()=>dispatch({type:CLEAR_CART})}>CLEAR CART</button>
              </div>
         </div>
         <div>
@@ -61,6 +80,8 @@ setPrice(0);
             state.map((a)=>(
             <div>
               <h1>{a.name}</h1>
+              <button onClick={()=>handleDelete(a.id)}>Delete</button>
+              <input type="number" value={a.quantity} onChange={(e)=>handleChange(a.id,Number(e.target.value))}  min='1'/>
               </div>
             ))
           }
