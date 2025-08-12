5bcfe11ec3002cbdc9679edb15754d53d15e713b
Uday bandari <udaybandari593@gmail.com>
2025-08-12 18:03:03 +0530

    updated

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 const ADD_ITEM="ADD_ITEM";
 const REMOVE_ITEM="REMOVE_ITEM";
 const CHANGE_QUANTITY="CHANGE_QUANTITY";
@@ -15,17 +15,20 @@ const ShoppingCart = () => {
     // const handleAdd=()=>{
     //     dispatch({type:ADD_ITEM,payload:})
     // }
+    const [name,setName]=useState("");
+    const[quantity,setQuantity]=useState(0);
+    const[price,setPrice]=useState(0);
   return (
     <div className="m-22 grid grid-cols-2">
         <div className="flex flex-col gap-4">
             <button  className="border-2 w-33 rounded-md px-2 py-1 cursor-pointer">Add to Cart</button> 
              <div className="flex  flex-col gap-4">
                 <label >NAME:</label>
-             <input className="w-48 focus:outline-none text-[15px]" placeholder="Enter the name of product" type="Text" min='1' step='1'  />
+             <input className="w-48 focus:outline-none text-[15px]" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter the name of product" type="Text"  />
                    <label >Quantity:</label>
-             <input className="w-33 focus:outline-none text-[15px]" placeholder="Enter quantity.." type="number" min='1' step='1'  />
+             <input className="w-33 focus:outline-none text-[15px]" value={quantity}  onChange={(e)=>setQuantity(e.target.value)}placeholder="Enter quantity.." type="number" min='1' step='1'  />
                <label >ADD PRICE:</label>
-             <input className="w-33 focus:outline-none text-[15px]" placeholder="Enter price.." type="number" min='1' step='1'  />
+             <input className="w-33 focus:outline-none text-[15px]"  value={price}  onChange={(e)=>setPrice(e.target.value)} placeholder="Enter price.." type="number" min='1' step='1'  />
              
              </div>
         </div>
