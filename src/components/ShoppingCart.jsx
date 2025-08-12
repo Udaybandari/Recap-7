b33590a1dfb2fcd37011dda795e7d310545d3a3a
Uday bandari <udaybandari593@gmail.com>
2025-08-12 17:57:12 +0530

    updated

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -19,9 +19,14 @@ const ShoppingCart = () => {
     <div className="m-22 grid grid-cols-2">
         <div className="flex flex-col gap-4">
             <button  className="border-2 w-33 rounded-md px-2 py-1 cursor-pointer">Add to Cart</button> 
-             <div className="flex gap-4">
+             <div className="flex  flex-col gap-4">
+                <label >NAME:</label>
+             <input className="w-48 focus:outline-none text-[15px]" placeholder="Enter the name of product" type="Text" min='1' step='1'  />
                    <label >Quantity:</label>
-             <input className="w-13 focus:outline-none text-[15px]" type="number" min='1' step='1' />
+             <input className="w-33 focus:outline-none text-[15px]" placeholder="Enter quantity.." type="number" min='1' step='1'  />
+               <label >ADD PRICE:</label>
+             <input className="w-33 focus:outline-none text-[15px]" placeholder="Enter price.." type="number" min='1' step='1'  />
+             
              </div>
         </div>
         <div>
