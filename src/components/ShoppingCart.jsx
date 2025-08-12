ba54dcdc928372f694d4f98683244ae6a5695085
Uday bandari <udaybandari593@gmail.com>
2025-08-12 18:13:15 +0530

    feature added

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,25 +3,49 @@ const ADD_ITEM="ADD_ITEM";
 const REMOVE_ITEM="REMOVE_ITEM";
 const CHANGE_QUANTITY="CHANGE_QUANTITY";
 const CLEAR_CART="CLEAR_CART";
-// function reducer(state,action)
-// {
-//     switch(action.type)
-//     {
-     
-//     }
-// }
+function reducer(state,action)
+{
+    switch(action.type)
+    {
+      case ADD_ITEM:
+    return  [
+     ...state,
+     {
+      id:action.payload.id,
+      name:action.payload.name,
+      quantity:action.payload.quantity,
+      price:action.payload.price,
+      
+     }
+     ]
+     default:
+      return state;
+    }
+}
 const ShoppingCart = () => {
-    // const[state,dispatch]=useReducer(reducer,[]);
-    // const handleAdd=()=>{
-    //     dispatch({type:ADD_ITEM,payload:})
-    // }
+    const[state,dispatch]=useReducer(reducer,[]);
+    const handleAdd=()=>{
+          const object={
+  id: Date.now(),       
+  name: name,
+  quantity: Number(quantity),
+  price: Number(price)
+}
+        dispatch({type:ADD_ITEM,payload:object})
+        setName("");
+setQuantity(0);
+setPrice(0);
+
+    }
     const [name,setName]=useState("");
     const[quantity,setQuantity]=useState(0);
     const[price,setPrice]=useState(0);
+
+
   return (
     <div className="m-22 grid grid-cols-2">
         <div className="flex flex-col gap-4">
-            <button  className="border-2 w-33 rounded-md px-2 py-1 cursor-pointer">Add to Cart</button> 
+            <button  onClick={handleAdd} className="border-2 w-33 rounded-md px-2 py-1 cursor-pointer">Add to Cart</button> 
              <div className="flex  flex-col gap-4">
                 <label >NAME:</label>
              <input className="w-48 focus:outline-none text-[15px]" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter the name of product" type="Text"  />
@@ -33,7 +57,13 @@ const ShoppingCart = () => {
              </div>
         </div>
         <div>
-          
+          {
+            state.map((a)=>(
+            <div>
+              <h1>{a.name}</h1>
+              </div>
+            ))
+          }
         </div>
     </div>
   )
