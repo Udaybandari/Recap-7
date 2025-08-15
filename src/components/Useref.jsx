e2206e80495f30aabf5ccb0755bf5c6539eb038f
Uday bandari <udaybandari593@gmail.com>
2025-08-15 17:27:38 +0530

    updated

--- src/components/Useref.jsx
+++ src/components/Useref.jsx
@@ -12,18 +12,27 @@ const startTimer=()=>{
   if(!running)
   {
    setRunning(true);
-  Time.current= setInterval(()=>(
-    setTime((t)=>t+1)
-   ),1000)
+  Time.current= setInterval(()=>{
+setTime((t)=>t+1);
+console.log(Time.current)
+  }
+    ,1000)
+  
   }
 }
 
-
+const stopTimer=()=>{ 
+  clearInterval(Time.current)
+  prevTime.current=time;
+  setRunning(false);
+}
   return (
 
-    <div className="m-5">
-      <h1>{Time.current}</h1>
+    <div className="m-5 flex gap-5">
+      {prevTime.current!==null&&<h1>{prevTime.current}</h1>}
+      <h1>{time}</h1>
          <button onClick={startTimer}>Start</button>
+         <button onClick={stopTimer}>Stop</button>
     </div>
   )
 };
