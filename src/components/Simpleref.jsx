a5cf8ce529cdca224b3fc391ffee30d303713062
Uday bandari <udaybandari593@gmail.com>
2025-08-16 11:58:37 +0530

    updated

--- src/components/Simpleref.jsx
+++ src/components/Simpleref.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-const Simpleref = () => {
+const Simpleref = () => { 
+    const countRef=useRef(0);
+    const increment=()=>{
+        countRef.current+=1;
+        console.log(countRef.current) ;
+        console.log(divRef.current)
+    }
+    const divRef=useRef();
+    const inputRef=useRef();
+     useEffect(()=>{
+            setTimeout(()=>{
+    divRef.current.style.color="red";
+    },1000)
+    inputRef.current.focus();
+    return ()=>clearTimeout();
+     },[])
+    
+    
   return(
-    <div>
-       <h1>Hello</h1>
+    <div ref={divRef} className="m-33 ">
+       <h1 >Hello</h1>
+       <button className="border-2 px-2" onClick={increment}>Click</button>
+       <input type="text" ref={inputRef} />
     </div>
   )
 };
