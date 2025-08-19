978cd8cca25bd358739e0e247edad4ad02ade9fd
Uday bandari <udaybandari593@gmail.com>
2025-08-19 16:20:48 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import UseReducer from "./components/UseReducer";
-import UseReducer2 from "./components/UseReducer2";
-import AdvancedTodoApp from "./components/AdvancedTodoApp";
-import ShoppingCart from "./components/ShoppingCart";
-import Cart from "./components/Cart";
-import Usref from "./components/Useref";
-;
-import Us from "./components/Us";
-import Simpleref from "./components/simpleref";
-import CallMemo from "./components/CallMemo";
-import Callback from "./components/Callback";
+
 
 const App = () => {
   return (
     <div className="m-22 w-[1400px] h-[600px] border-1 flex flex-col items-center justify-center">
-      {/* <UseReducer/>
-      <UseReducer2/> 
-      <AdvancedTodoApp/>
-      <ShoppingCart/> */}
-      {/* <Cart/> */}
-      {/* <Usref/> */}
-  {/* <Us/> */}
-  {/* <Simpleref/> */}
-  <CallMemo/>
-  {/* <Callback/> */}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 };
