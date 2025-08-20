5ac9da83c050c55f028f7858f02c52cbf338f452
Uday bandari <udaybandari593@gmail.com>
2025-08-20 18:28:30 +0530

    updated

---
+++ src/components/ShoppingCart/Context/index.jsx
@@ -0,0 +1,19 @@
+  //create context
+  //provide the state to the context
+  //wrap context to root component
+  //consume the context using usecontext
+
+import { createContext } from "react";
+
+import useFetch from "../../Practiced/Customhook";
+  export const ShoppingCartContext=createContext(null);
+
+  function ShoppingContext({children})
+  {
+     const {data,loading,error}=useFetch("https://dummyjson.com/products");
+console.log(data);
+    return <ShoppingCartContext.Provider value={{data,loading,error}}>
+        {children}
+        </ShoppingCartContext.Provider>
+  }
+  export default ShoppingContext;
---
+++ src/components/ShoppingCart/ProductTile/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductTile = () => {
+  return <div>This is the one single product Tile</div>;
+};
+
+export default ProductTile;
