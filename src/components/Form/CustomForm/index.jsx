abd7c424efed2056bb6d93c1fb6ca499533728c6
Uday bandari <udaybandari593@gmail.com>
2025-08-28 20:59:05 +0530

    updated

--- src/components/Form/CustomForm/index.jsx
+++ src/components/Form/CustomForm/index.jsx
@@ -8,11 +8,56 @@ const CustomForm = ({
   setFormData,
   buttonText,
 }) => {
+function renderFormElement(getCurrentElement) {
+    let content = null;
 
+    switch (getCurrentElement?.componentType) {
+      case formTypes.INPUT:
+        content = (
+          <CommonInput
+            label={getCurrentElement.label}
+            name={getCurrentElement.name}
+            id={getCurrentElement.id}
+            type={getCurrentElement.type}
+            placeholder={getCurrentElement.placeholder}
+            value={formData[getCurrentElement.name]}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [event.target.name]: event.target.value,
+              })
+            }
+          />
+        );
+
+        break;
+
+      default:
+        content = (
+          <CommonInput
+            label={getCurrentElement.label}
+            name={getCurrentElement.name}
+            id={getCurrentElement.id}
+            placeholder={getCurrentElement.placeholder}
+            value={formData[getCurrentElement.name]}
+            type={getCurrentElement.type}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [event.target.name]: event.target.value,
+              })
+            }
+          />
+        );
+        break;
+    }
+
+    return content;
+  }
   return (
     <div>
         {loginFormElements.length?.map((a)=>
-          
+           renderFormElement(singleFormElementItem)
         )}
     </div>
   )
