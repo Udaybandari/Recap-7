c91292022650da2c4d4978172e6551080715d6ae
Uday bandari <udaybandari593@gmail.com>
2025-09-03 16:26:04 +0530

    updated

--- src/components/PROJECTS/Timer/test.jsx
+++ src/components/PROJECTS/Timer/test.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Timer from "./timer";
 
 const Main = () => {
-    const[value,setValue]=useState(0);
+    const[value,setValue]=useState();
     console.log(value);
   return (
     <div className="m-55 border-5 h-88 flex items-center justify-center flex-col">
         <h1>Timer</h1>
         <input type="number" value={value}  placeholder="enter the value" onChange={(e)=>{
-            setValue(e.target.value)
+            setValue(Number(e.target.value));
+
         }}/>
         <Timer initialTime={value}/>
     </div>
