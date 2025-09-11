ede005e86aae360b952a04775d86787157284627
Uday bandari <udaybandari593@gmail.com>
2025-09-11 20:38:57 +0530

    updated

--- src/components/PROJECTS/CurrencyConverter/index.jsx
+++ src/components/PROJECTS/CurrencyConverter/index.jsx
@@ -5,7 +5,7 @@ const Currency = () => {
     const[fromCurrency,setFromCurrency]=useState("USD");
     const[toCurrency,setToCurrency]=useState("INR");
     const[exchangeRate,setExchangeRate]=useState();
-    const[converterAmount,setConvertedAmount]=useState();
+    const[convertedAmount,setConvertedAmount]=useState();
     async function fetchexchangeRate(){
         const Response=await fetch(
             `https://open.er-api.com/v6/latest/${fromCurrency}`,
@@ -24,6 +24,13 @@ const Currency = () => {
      function handleAmountChange(event) {
     setAmount(event.target.value);
   }
+    function handleFromCurrencyChange(event) {
+    setFromCurrency(event.target.value);
+  }
+    function handleToCurrencyChange(event) {
+    setToCurrency(event.target.value);
+  }
+  console.log(amount)
 
   useEffect(() => {
     fetchexchangeRate();
@@ -39,6 +46,23 @@ const Currency = () => {
           name="amount"
           placeholder="Enter Amount"
         />
+        <select value={fromCurrency} onChange={handleFromCurrencyChange}>
+          <option value={"USD"}>USD</option>
+          <option value={"INR"}>INR</option>
+          <option value={"EUR"}>EUR</option>
+        </select>
+
+        </div>
+        <h1>To</h1>
+        <div>
+             <div className="input-container">
+        <input type="text" value={convertedAmount} readOnly />
+        <select value={toCurrency} onChange={handleToCurrencyChange}>
+          <option value={"EUR"}>EUR</option>
+          <option value={"INR"}>INR</option>
+          <option value={"USD"}>USD</option>
+        </select>
+      </div>
         </div>
     </div>
   )
